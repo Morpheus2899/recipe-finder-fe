9c28f7eba0ad4681298849d96d52e0ce1af39d0e
Morpheus2899 <francescospreafico1@gmail.com>
2025-01-18 20:34:44 +0100

    add color palette and other changes

---
+++ src/assets/color-palette.ts
@@ -0,0 +1,9 @@
+const colorPalette = {
+    creme: "#fefae0",
+    green: "#ccd5ae",
+    light_green: "#e9edc9",
+    brown: "#d4a373",
+    light_brown: "#faedcd",
+};
+
+export default colorPalette;
