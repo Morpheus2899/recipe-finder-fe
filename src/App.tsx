ac9e5cf0a479cc9b63e9a6d23766e0d4fe9de111
Morpheus2899 <francescospreafico1@gmail.com>
2025-01-19 19:10:23 +0100

    first concept of search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,86 @@
 import styled from "styled-components"
 import colorPalette from "./assets/color-palette";
+import SearchBar from "./components/search-bar";
 
 const MainContainer = styled.div`
+    background-color: ${colorPalette.creme};
+    display: flex;
+    flex-direction: column;
+	width: 100vw;
+	height: 100vh;
+`;
+
+const Container = styled.div`
 	background-color: ${colorPalette.green};
   	display: flex;
 	flex-direction: column;
-  	width: 100vw;
-  	height: 100vh;
+	align-items: center;
 `;
 
 function App() {
 
 	return (
-    	<MainContainer>
-    		<p>ciao</p>
-      		<input type="text" />
-    	</MainContainer>
+		<MainContainer>
+			<Container>
+				<SearchBar/>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+				<h1>ciao</h1>
+    		</Container>
+		</MainContainer>
   	)
 }
 
---
+++ src/components/search-bar.tsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import colorPalette from "../assets/color-palette";
+// import { ReactComponent as SearchIcon } from "../assets/icons/search.svg";
+import SearchIcon from "../assets/icons/search.svg";
+import CrossIcon from "../assets/icons/x.svg";
+
+const MainContainer = styled.div`
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 80vw;
+    height: 8rem;
+    background-color: ${colorPalette.green};
+    box-shadow: 0px 10px 10px 10px ${colorPalette.green};
+`;
+
+const SearchBarContainer = styled.div`
+    border-radius: 100px;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    height: 4rem;
+    width: 100%;
+    margin: 0 2rem;
+    background-color: ${colorPalette.light_green};
+`;
+
+const SearchBarStyled = styled.input`
+    background-color: ${colorPalette.light_green};
+    border: 0px solid;
+    outline: none;
+    height: 3rem;
+    width: 100%;
+    font-size: 1.1rem;
+`;
+
+const SearchBar = () => {
+    return (
+        <MainContainer>
+            <div style={{
+                width: "200px",
+                height: "3rem",
+                backgroundColor: "red",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "10px",
+                color: "white",
+            }}>LOGO</div>
+            <SearchBarContainer>
+                {/* <SearchIcon/> */}
+                <img src={SearchIcon} alt="icon" style={{margin: "0 1rem"}}/>
+                <SearchBarStyled type="text" placeholder="Search ingredients ..." />
+                <img src={CrossIcon} alt="icon" style={{margin: "0 1rem"}}/>
+            </SearchBarContainer>
+            <div style={{
+                backgroundColor: "yellow",
+            }}>
+
+            </div>
+        </MainContainer>
+    )
+}
+
+export default SearchBar;
